d412401bd349b08721fe7807bbad8c89ddd74dbc
zilnur <86298777+zilnur@users.noreply.github.com>
2023-08-30 15:48:15 +0300

    Initial Commit

---
+++ BookingApp/BookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingAppApp.swift
+//  BookingApp
+//
+//  Created by Ильнур Закиров on 30.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
